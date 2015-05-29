df08ec59fa8fc71d09ff8bb25e07ec6721f41a1f
Bryan Clark <clarkbw@gmail.com>
2015-05-29 10:48:38 -0700

    initial mixin example
    Using two types, a local mixin loaded from a file
    Also using a module mixin loaded from node_modules

---
+++ test/test.js
@@ -0,0 +1,17 @@
+
+
+describe('mixin example', function() {
+  'use strict';
+
+  it('should load the module mixin', function() {
+    console.log(app.models.Dog.definition.properties);
+    assert.ok(app.models.Dog.definition.properties.createdAt,
+      'missing createdAt');
+    assert.ok(app.models.Dog.definition.properties.updatedAt,
+      'missing updatedAt');
+  });
+
+  it('should load the local mixin', function() {
+    assert.ok(app.models.Dog.definition.properties.tag);
+  });
+});
